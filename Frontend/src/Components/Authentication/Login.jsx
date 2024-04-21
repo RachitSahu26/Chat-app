2cf3a03dbc5b2c19f55cf1a33a1ca9629eb3b4d3
Rachit Sahu <curiousrachit.26@gmail.com>
2024-04-21 23:12:04 +0530

    creating login funtionality

--- Frontend/src/Components/Authentication/Login.jsx
+++ Frontend/src/Components/Authentication/Login.jsx
@@ -1,69 +1,90 @@
 import React, { useState } from 'react'
 
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack }  from "@chakra-ui/react";
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from "@chakra-ui/react";
 function Login() {
-    const [show, setShow] = useState(false);
-    const handleClick = () => setShow(!show);
-
-
-    const [email,setEmail]=useState("");
-    const [password,setPassword]=useState("");
 
 
 
-    const submitHandler=()=>{
+  const [signupData, setSignupData] = useState({
+    name: "",
+    email: "",
+    password: "",
 
-    }
+  });
+
+
+  const handleUserInput = (e) => {
+
+    const { name, value } = e.target;
+    setSignupData({
+      ...signupData,
+      [name]: value
+    })
+  }
+
+
+
 
   return (
     <VStack spacing="5px">
-   
+
+
+
+      {/* ..........Email....... */}
+      <FormControl id="email" isRequired>
+        <FormLabel>Email Address</FormLabel>
+        <Input
+          type="email"
+          name="name"
+          placeholder="Enter Your Email Address"
+         
+          value={signupData.name}
+          onChange={handleUserInput}
+        />
+      </FormControl>
+
+      {/* ..........Password....... */}
+
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter Password"
+
     
-    
-     {/* ..........Email....... */}
-     <FormControl id="email" isRequired>
-       <FormLabel>Email Address</FormLabel>
-       <Input
-         type="email"
-         placeholder="Enter Your Email Address"
-         onChange={(e) => setEmail(e.target.value)}
-       />
-     </FormControl>
-   
-    {/* ..........Password....... */}
-   
-     <FormControl id="password" isRequired>
-       <FormLabel>Password</FormLabel>
-       <InputGroup size="md">
-         <Input
-           type={show ? "text" : "password"}
-           placeholder="Enter Password"
-           onChange={(e) => setPassword(e.target.value)}
-         />
-         <InputRightElement width="4.5rem">
-           <Button h="1.75rem" size="sm" onClick={handleClick}>
-             {show ? "Hide" : "Show"}
-           </Button>
-         </InputRightElement>
-       </InputGroup>
-     </FormControl>
-    
-    
-   
-    {/* ..........signup btn....... */}
-     <Button
-       colorScheme="blue"
-       width="100%"
-       style={{ marginTop: 15 }}
-       onClick={submitHandler}
- 
-     >
-       Login
-     </Button>
-   
-   
-   
-   </VStack>
+            value={signupData.password}
+            onChange={handleUserInput}
+
+
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+
+
+
+      {/* ..........signup btn....... */}
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+
+
+
+
+      >
+        Login
+      </Button>
+
+
+
+    </VStack>
   )
 }
 
