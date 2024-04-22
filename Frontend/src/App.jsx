e9541e33ca9e64bb6480364bedfe3788825cdb8a
Rachit Sahu <curiousrachit.26@gmail.com>
2024-04-22 23:12:31 +0530

    Add bcrypt, cors, and other dependencies; update package-lock.json
    This commit adds bcrypt and cors to the project dependencies, along with several other packages added as dependencies of those packages. The package-lock.json

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Chat from './Pages/Chat';
 import Home from './Pages/Home';
-
+import toast, { Toaster } from 'react-hot-toast';
 
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/chat" element={<Chat />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+      <Toaster />
+    </>
   );
 }
 
