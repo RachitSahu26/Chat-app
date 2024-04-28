c68bde4799ecdf835f7719d0d2aba4bc951e66f3
Rachit Sahu <curiousrachit.26@gmail.com>
2024-04-28 13:56:27 +0530

    changed model of chat

--- backend/Models/chatModel.js
+++ backend/Models/chatModel.js
@@ -1,19 +1,16 @@
 const mongoose = require("mongoose");
+const chatModel = new mongoose.Schema({
+    participants:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }],
+    messages:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Message"
+    }]
+},{timestamps:true});
 
-const chatModel = mongoose.Schema(
-  {
-    chatName: { type: String, trim: true },
-    isGroupChat: { type: Boolean, default: false },
-    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    latestMessage: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Message",
-    },
-    groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  },
-  { timestamps: true }
-);
 
-const Chat = mongoose.model("Chat", chatModel);
+const chat = mongoose.model("Chat", chatModel);
 
-module.exports = Chat;
+module.exports = chat;
