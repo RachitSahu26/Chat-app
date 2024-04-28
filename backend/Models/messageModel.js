33d1c71934065a94b9e438742bd173dd5afa2dbc
Rachit Sahu <curiousrachit.26@gmail.com>
2024-04-28 13:56:43 +0530

    changed message model

--- backend/Models/messageModel.js
+++ backend/Models/messageModel.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
-
-const messageSchema = mongoose.Schema(
-  {
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    content: { type: String, trim: true },
-    chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
-    readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  },
-  { timestamps: true }
-);
-
-const Message = mongoose.model("Message", messageSchema);
-module.exports = Message;
+const messageModel = new mongoose.Schema({
+    senderId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    receiverId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    message:{
+        type:String,
+        required:true
+    }
+},{timestamps:true});
+ const Message = mongoose.model("Message", messageModel);
+ module.exports = Message;
