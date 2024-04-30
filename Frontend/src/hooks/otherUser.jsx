ef33a851e4fb08c2d9ac71ab0f96318a751ac00d
Rachit Sahu <curiousrachit.26@gmail.com>
2024-04-30 21:02:29 +0530

    hooks of the otherUser

---
+++ Frontend/src/hooks/otherUser.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setOtherUsers } from '../redux/userSlice';
+
+
+const otherUser = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchOtherUsers = async () => {
+            try {
+                axios.defaults.withCredentials = true;
+                const res = await axios.get(`http://localhost:4040/api/user/`);
+                // store
+                console.log("other users -> ",res);
+                dispatch(setOtherUsers(res.data));
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchOtherUsers();
+    }, [])
+
+}
+
+export default otherUser
