d6483fbc76423520504a2b426f4aeeb9bd15b172
Rachit Sahu <curiousrachit.26@gmail.com>
2024-05-09 23:37:36 +0530

    massage render

--- Frontend/src/Components/MessageContainer/MessageArea.jsx
+++ Frontend/src/Components/MessageContainer/MessageArea.jsx
@@ -1,36 +1,27 @@
 import React from 'react'
 import Message from '../Authentication/Message'
 import SendChatForm from './SendChatForm'
+import { useSelector } from 'react-redux'
 
 function MessageArea() {
+
+    const {message} = useSelector((state) => state.messages);
+    if (!message) {
+      console.log( "No message found.");
+    }
     return (
         < >
             {/* ..............message............ */}
 
             <div className=' h-[80%] overflow-y-auto'>
 
+                {
+                   message && message?.map((mess) => (
+                        <Message key={mess._id} message={mess} />
+                    ))
+                }
 
-                <Message />
-                <Message />
-                <Message />
-                <Message />
-                <Message />
-                <Message />
-                <Message />
 
-                <Message />
-                <Message />
-                <Message />
-                <Message />
-                <Message />   <Message />
-                <Message />
-                <Message />
-                <Message />
-                <Message />   <Message />
-                <Message />
-                <Message />
-                <Message />
-                <Message />
             </div>
 
 
