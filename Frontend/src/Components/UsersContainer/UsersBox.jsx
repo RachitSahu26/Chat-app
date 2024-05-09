11a2c915387ee82ec079a587d9c3d62734c66ec8
Rachit Sahu <curiousrachit.26@gmail.com>
2024-05-09 23:37:12 +0530

    render the user

--- Frontend/src/Components/UsersContainer/UsersBox.jsx
+++ Frontend/src/Components/UsersContainer/UsersBox.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import User from './User'
+import { useSelector } from 'react-redux';
 
 function UsersBox() {
+
+  const otherUser = useSelector(state => state.user.otherUserData);
+
+
+  // Check if otherUser is an array
+  if (!Array.isArray(otherUser)) {
+    // Handle the case where otherUser is not an array
+    return <p>No other users found.</p>;
+  }
+
+
   return (
     <div>
-      <User/>
-     
+      {
+        otherUser.map((user) => (
+          <User key={user._id} user={user} />
+        ))
+
+      }
+
+
 
     </div>
   )
