23324d6f07bb036e002a55789f504f0bb3515990
Rachit Sahu <curiousrachit.26@gmail.com>
2024-05-10 22:40:42 +0530

    added socket

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -4,11 +4,13 @@ import { config } from "dotenv";
 
 import userSlice from "./Slice/user.Slice";
 import messageSlice from "./Slice/message.Slice";
+import socketSlice from "./Slice/socket.Slice";
 
 const store = configureStore({
     reducer: {
         user: userSlice,
-        messages: messageSlice
+        messages: messageSlice,
+        socket: socketSlice,
     },
     devTools: true
 })
