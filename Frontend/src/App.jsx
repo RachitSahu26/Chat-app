eb38446b895bd21b40ec291cac0628595811e532
Rachit Sahu <curiousrachit.26@gmail.com>
2024-05-10 22:41:31 +0530

    connected socket

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Chat from './Pages/Chat';
 import Home from './Pages/Home';
 import toast, { Toaster } from 'react-hot-toast';
-
+import { useDispatch, useSelector } from 'react-redux';
+import io from "socket.io-client";
+import { storeSocketData } from './redux/Slice/socket.Slice';
+import { storeOnlineUser } from './redux/Slice/user.Slice';
 
 function App() {
+
+  const dispatch = useDispatch();
+  const { authdata } = useSelector((state) => state.user)
+  const [socket, setSocket] = useState(null);
+  useEffect(() => {
+    if (authdata) {
+
+      const socket = io('http://localhost:4040', {
+        query: {
+          authId: authdata._id
+        },
+      });
+
+
+      dispatch(storeSocketData(socket));
+      socket.on("getOnlineUsers", (onlineUsers) => {
+        dispatch(storeOnlineUser(onlineUsers));
+      })
+
+
+
+    }
+
+  }, [authdata])
+
+
   return (
     <>
       <Routes>
