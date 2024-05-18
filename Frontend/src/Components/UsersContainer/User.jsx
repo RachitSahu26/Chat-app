16c03e77eaea6d556d0ea5844a7e97666742e8ef
Rachit Sahu <curiousrachit.26@gmail.com>
2024-05-18 22:22:04 +0530

    something change in the responsive

--- Frontend/src/Components/UsersContainer/User.jsx
+++ Frontend/src/Components/UsersContainer/User.jsx
@@ -1,64 +1,48 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { storeSelectedUser } from '../../redux/Slice/user.Slice';
+import { useNavigate } from 'react-router-dom';
+// import { useMediaQuery } from '@react-hook/media-query'; // Import useMediaQuery
 
 function User(props) {
-    const userData = props.user
+    const userData = props.user;
 
     const dispatch = useDispatch();
-
-    const selectedUsser = useSelector(state => state.user.selectedUser);
+    const navigate = useNavigate();
+    const selectedUser = useSelector(state => state.user.selectedUser);
     const { onlineUser } = useSelector((state) => state.user);
-
-
     const isOnline = onlineUser?.includes(userData._id);
 
-   
-    // ..................selected user.........
-    const selectedUser = (selectData) => {
+    // Use the useMediaQuery hook to get the current screen size
+    // const isMobile = useMediaQuery('(max-width: 768px)');
 
-        dispatch(storeSelectedUser(selectData))
-    }
+    // Function to handle user selection based on screen size
+    const handleUserSelection = (selectData) => {
+
+            dispatch(storeSelectedUser(selectData));
+     
+    };
 
     return (
         <>
-            {/* Map through otherUser array and render each user */}
-
             <div
-                onClick={() => selectedUser(userData)}
-                className={`${selectedUsser?._id === userData?._id ? 'bg-black  ' : ''
-                    } border-2 m-2 flex p-2 hover:bg-black hover:border-5 hover:border-yellow-600    cursor-pointer border-black h-20 rounded-lg`}
+                onClick={() => handleUserSelection(userData)}
+                className={`${selectedUser?._id === userData?._id ? 'bg-black' : ''
+                    } h-auto border-2 m-2 flex p-2 hover:bg-black hover:border-5 hover:border-yellow-600 cursor-pointer border-white  rounded-lg`}
             >
-                <div className={` ${selectedUsser?._id === userData?._id ? 'text-yellow-500 ' : 'text-white'} flex text-white   hover:text-white w-full `}>
-                    {/* Assuming otherUserData contains an image URL */}
-
+              
+              
+                <div className={`${selectedUser?._id === userData?._id ? 'text-yellow-500' : 'text-white'} p-3 h-auto   flex text-white hover:text-white w-full flex-col md:flex-row `}>
                     <div className={`avatar ${isOnline ? 'online' : ''}`}>
                         <div className='w-12 rounded-full'>
                             <img src={userData.profilePhoto} alt="user-profile" />
                         </div>
                     </div>
-
-
-
-
-
-
-
-
-
-
-
-
-                    <div className=' ml-3 p-1 w-full '>
-                        {/* Assuming otherUserData contains name and description */}
-                        <h2>{userData.fullName}</h2>
-                        {/* <p className='text-[10px]'>{user.description}</p> */}
+                    <div className=' h-auto  item-center p-1 w-full '>
+                        <h2 className=' text-[10px] sm:text-xl '>{userData.fullName}</h2>
                     </div>
-
                 </div>
-
             </div>
-
         </>
     );
 }
