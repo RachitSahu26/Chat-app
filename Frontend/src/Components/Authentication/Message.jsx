d3bfe0173451e3cf670b4dd113ff738dc510960e
Rachit Sahu <curiousrachit.26@gmail.com>
2024-05-23 09:46:40 +0530

    changed some conditon

--- Frontend/src/Components/Authentication/Message.jsx
+++ Frontend/src/Components/Authentication/Message.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useSelector } from 'react-redux'
+// import fetchMessage from '../../hooks/fetchMessage';
+
 
 function Message({ message }) {
   const { authdata, selectedUser } = useSelector((state) => state.user);
+  const scroll = useRef();
+console.log("messages of the main ", message);
+  useEffect(() => {
+    scroll.current?.scrollIntoView({ behavior: "smooth" });
+    
+
+    
+  }, [message]);
+
+
+
 
   return (
 
 
 
-    <div className={`chat p-4  ${authdata?._id === message?.senderId ? 'chat-end' : 'chat-start'} `}>
+    <div ref={scroll} className={`chat p-4  ${authdata?._id === message?.senderId ? 'chat-end' : 'chat-start '} `}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
           <img alt="Tailwind CSS chat bubble component" src={`${message?.senderId === authdata?._id ? authdata?.profilePhoto : selectedUser?.profilePhoto}`} />
         </div>
       </div>
-      <div className="chat-header">
-        Obi-Wan Kenobi
-        <time className="text-xs opacity-50">12:45</time>
-      </div>
-      <div className="chat-bubble">{message?.message}</div>
+  
+      <div className={`${authdata?._id === message?.senderId ? 'chat-bubble-info p-2 rounded-lg' : 'chat-bubble-warning p-2 rounded-lg '}`}>{message?.message}</div>
+
 
     </div>
 
