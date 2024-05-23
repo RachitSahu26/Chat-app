2ab0f52eebc97da2aaecbcfefdd1a91916a4c1a2
Rachit Sahu <curiousrachit.26@gmail.com>
2024-05-23 09:43:29 +0530

    provide dependency in the useEffect

--- Frontend/src/hooks/fetchMessage.jsx
+++ Frontend/src/hooks/fetchMessage.jsx
@@ -3,23 +3,23 @@ import { useDispatch, useSelector } from 'react-redux'
 import axios from "axios";
 import { storedMessageData } from '../redux/Slice/message.Slice';
 const fetchMessage = () => {
-const dispatch= useDispatch();
-    const selectedUer = useSelector((state) => state.user.selectedUser);
-    // console.log("goted from the store using UseSelector", selectedUer);
-    useEffect(() => {
-        const userMessage = async () => {
-            try {
-                const res = await axios.get(`http://localhost:4040/api/message/${selectedUer?._id}`);
-                // console.log(res.data);
-                dispatch(storedMessageData(res.data))
-            } catch (error) {
-                console.log(error);
+  const dispatch = useDispatch();
+  const {selectedUser} = useSelector((state) => state.user);
+  // console.log("goted from the store using UseSelector", selectedUer);
+  useEffect(() => {
+    const userMessage = async () => {
+      try {
+        const res = await axios.get(`http://localhost:4040/api/message/${selectedUser?._id}`);
+        // console.log(res.data);
+        dispatch(storedMessageData(res.data))
+      } catch (error) {
+        console.log(error);
 
-            }
+      }
 
-        }
-        userMessage();
-    }, [selectedUer])
+    }
+    userMessage();
+  }, [selectedUser])
 
 
 
